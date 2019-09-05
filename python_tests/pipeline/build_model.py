a1240dc0b5134e6c1829cf7437acb3daa1459f9d
tompreece <tom.preece@peak.ai>
2019-09-05 10:51:45 +0100

    Added auc

--- python_tests/pipeline/build_model.py
+++ python_tests/pipeline/build_model.py
@@ -75,6 +75,8 @@ print(grid_search.best_params_, grid_search.best_score_)
 
 y_pred = grid_search.best_estimator_.predict_proba(X_test)
 
+auc = roc_auc_score(y_true=y_test, y_score=y_pred[:,1])
+print(auc)
 print("Test set AUC: ", roc_auc_score(y_true=y_test, y_score=y_pred[:,1]))
 
 # Dump best model to S3
---
+++ python_tests/pipeline/deploy_model.py
@@ -0,0 +1,1 @@
+print("Model deployment could happen here")
